23ad83d22b099c80616edf513ffb21608471d985
Austin Minnon and Michael Dada <whi7ee@gmail.com>
2016-01-21 09:32:07 -0800

    add spec for countUp function

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,3 @@ if ((countBy !== 0) || (countBy!==NaN) || (countTo !== 0) || (countTo!==NaN)) {
   return false;
 };
 };
-
-// var isZero = function(countTo, countBy){
-//   //countBy
-//   //we want to analyze countTo, and if zero or NaN, return 'not a multiple'
-// if ((countTo !== 0) || (countTo!==NaN)) {
-//   return true;
-// }else{
-//   return false;
-// }
-// };
--- spec/specs.js
+++ spec/specs.js
@@ -4,6 +4,10 @@ describe('isZero', function(){
   });
   it("will return message 'not a multiple' if second number is zero or NaN", function(){
     expect(isZero(25, 0)).to.equal(true);
-
   });
+});
+describe('countUp', function(){
+  it("count to first input",function(){
+    expect(countUp(5,5)).to.equal("1,2,3,4,5");
+});
 });
