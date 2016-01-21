63c5241231ab9962346b4a2627be77e3abf64903
Austin Minnon and Michael Dada <whi7ee@gmail.com>
2016-01-21 09:16:09 -0800

    add script to check if either number is a zero

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-var isZero = function(countTo,countBy){
+var isZero = function(countTo, countBy){
+  //countTo
   //we want to analyze countBy, and if zero or NaN, return 'not a multiple'
-if (countBy === 0 || NaN) {
-  return false;
-}else {
+if ((countBy !== 0) || (countBy!==NaN)) {
   return true;
+}else {
+  return false;
 };
+};
+
+var isZero = function(countTo, countBy){
+  //countBy
+  //we want to analyze countTo, and if zero or NaN, return 'not a multiple'
+if ((countTo !== 0) || (countTo!==NaN)) {
+  return true;
+}else{
+  alert("weirdd")
+}
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('isZero', function(){
-  it("will return message 'not a multiple' if second number is zero", function(){
-    expect(isZero(25,0)).to.equal(false);
+  it("will return message 'not a factor' if first number is zero or NaN ", function(){
+    expect(isZero(0, 5)).to.equal(true);
+  });
+  it("will return message 'not a multiple' if second number is zero or NaN", function(){
+    expect(isZero(25, 0)).to.equal(true);
+
   });
 });
