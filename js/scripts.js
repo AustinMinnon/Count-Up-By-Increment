f74c664c1ce4cedafbaa7671f1994e3f9195f4e2
Austin Minnon and Michael Dada <mail.michaeldada@gmail.com>
2016-01-21 15:50:09 -0800

    add front end, change some logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var isZero = function(countTo, countBy){
   //countTo
   //we want to analyze countBy, and if zero or NaN, return 'not a multiple'
-  if ((countBy !== 0) || (countBy!==NaN) || (countTo !== 0) || (countTo!==NaN)) {
+  if ((countBy !== 0) && (isNaN(countBy) === false) && (countTo !== 0) && (isNaN(countTo) === false)) {
     return true;
   }else {
     return false;
@@ -11,9 +11,27 @@ var isZero = function(countTo, countBy){
 var countUp = function(countTo, countBy){
 // loop unti countTo number is reached
 var results = [];
-  for( var i=0; i<= countTo; i+=countBy){
+  for(var i = countBy; i <= countTo; i += countBy){
     results.push(i);
   }
-  var resultsSliced = results.slice(1,results.length)
-  return resultsSliced;
+
+
+  return results;
 };
+
+
+$(document).ready(function(){
+  $(".userInput").submit(function(event){
+    var input1 = parseInt($("input#countTo").val());
+    var input2 = parseInt($("input#countBy").val());
+    var filterNan = isZero(input1,input2);
+    var result = countUp(input1,input2);
+    if (filterNan === true) {
+      $(".result").text(result);
+    } else {
+      alert('please enter real numbers greater than zero');
+      return
+    }
+    event.preventDefault();
+  })
+})
