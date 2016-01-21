55497da0e99dc03f2d61a554645ce25c1f6066c1
Austin Minnon and Michael Dada <mail.michaeldada@gmail.com>
2016-01-21 09:21:04 -0800

    refractor scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 var isZero = function(countTo, countBy){
   //countTo
   //we want to analyze countBy, and if zero or NaN, return 'not a multiple'
-if ((countBy !== 0) || (countBy!==NaN)) {
+if ((countBy !== 0) || (countBy!==NaN) || (countTo !== 0) || (countTo!==NaN)) {
   return true;
 }else {
   return false;
 };
 };
 
-var isZero = function(countTo, countBy){
-  //countBy
-  //we want to analyze countTo, and if zero or NaN, return 'not a multiple'
-if ((countTo !== 0) || (countTo!==NaN)) {
-  return true;
-}else{
-  alert("weirdd")
-}
-};
+// var isZero = function(countTo, countBy){
+//   //countBy
+//   //we want to analyze countTo, and if zero or NaN, return 'not a multiple'
+// if ((countTo !== 0) || (countTo!==NaN)) {
+//   return true;
+// }else{
+//   return false;
+// }
+// };
